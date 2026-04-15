2c2bc6cfb3f2fd6c7596c0c2de62e0f02716c687
clai-12 <clai@bu.edu>
2026-04-14 20:25:32 -0400

    HTML + beginning CSS and JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,18 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+
+
+
+const oceanSunfishButton = document.getElementsByClassName("species" "ocean-sunfish");
+
+oceanSunfishButton.addEventListener('click') {
+console.log "hi";
+}
+
+function showOceanSunfishPopup (){
+let popup = getElementsByClassName("ocean-sunfish");
+if (popup.style.display === "none") {
+    popup.style.display = "block";
+  } else {
+    popup.style.display = "none";
+  }
+}
